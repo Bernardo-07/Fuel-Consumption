17ef87a7518d38ce4c9fcb328dd822e4a5c0fb09
Bernardo <bernardoduarte271@gmail.com>
2024-11-11 15:12:21 -0300

    error in normalization with tensorflow

--- test.py
+++ test.py
@@ -1,10 +1,28 @@
 import pandas as pd
+import numpy as np
+import tensorflow as tf
 
 file_path = "auto-mpg.csv"
 
 data = pd.read_csv(file_path)
 
-train_data = data.sample(frac=0.8, random_state=42)  # Pega 80% dos dados
+train_data = data.sample(frac=0.8, random_state=42)
 test_data = data.drop(train_data.index)      
 
-print(data)
+X_train = train_data.iloc[:, 1:-1]
+print(X_train)
+y_train = train_data.iloc[:, 0]
+
+X_test = test_data.iloc[:, 1:-1]
+y_test = test_data.iloc[:, 0]
+
+X_train['horsepower'] = pd.to_numeric(X_train['horsepower'])
+x = np.array(X_train)
+print(x)
+
+
+normalizer = tf.keras.layers.Normalization()
+normalizer.adapt(np.array(X_train))
+data_normalized = normalizer(np.array(X_train))
+
+print(data_normalized)
