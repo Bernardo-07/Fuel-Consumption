f902fc8c3f5d5fdf3354f46eb668abc63508af64
Bernardo <bernardoduarte271@gmail.com>
2024-11-18 15:04:02 -0300

    error fixed

--- test.py
+++ test.py
@@ -6,20 +6,20 @@ file_path = "auto-mpg.csv"
 
 data = pd.read_csv(file_path)
 
+for index, value in enumerate(data["horsepower"]):
+        if value == "?":  # Verifica se o valor é "?"
+            data.at[index, "horsepower"] = None  # Substitui por None (convertido para NaN)
+            data = data.dropna()
+data["horsepower"] = pd.to_numeric(data["horsepower"], errors='coerce')
+
 train_data = data.sample(frac=0.8, random_state=42)
 test_data = data.drop(train_data.index)      
 
 X_train = train_data.iloc[:, 1:-1]
-print(X_train)
 y_train = train_data.iloc[:, 0]
 
 X_test = test_data.iloc[:, 1:-1]
 y_test = test_data.iloc[:, 0]
-
-X_train['horsepower'] = pd.to_numeric(X_train['horsepower'])
-x = np.array(X_train)
-print(x)
-
 
 normalizer = tf.keras.layers.Normalization()
 normalizer.adapt(np.array(X_train))
